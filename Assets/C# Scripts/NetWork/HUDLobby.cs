6532c31a8a672f309a8ebd091f868d7219151c13
Егор Кучук <KriptYashka@gmail.com>
2020-08-08 15:25:26 +0300

    Первый HUD для подключения Erebor

---
+++ Assets/C# Scripts/NetWork/HUDLobby.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class HUDLobby : MonoBehaviour
+{
+    NetworkManager manager;
+
+    void Awake()
+    {
+        manager = GetComponent<NetworkManager>();
+    }
+
+    public void StartServerErebor()
+    {
+        if (Application.platform != RuntimePlatform.WebGLPlayer)
+        {
+                manager.StartHost();
+        }
+    }
+
+    public void StartServerCity()
+    {
+        return;
+    }
+
+    public void StartServerCrater()
+    {
+        return;
+    }
+}
