5983401b0427c4b4f16f5aa84f6088c0a0b33c27
Anuar-Azat <62546054+Anuar-Azat@users.noreply.github.com>
2020-08-09 14:20:00 +0600

    Работаю на ShowTank

---
+++ Assets/C# Scripts/ShowTanks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTanks : MonoBehaviour
+{
+   private List<Transform> models;
+
+   private void Awake()
+   {
+      models = new List<Transform>();
+      for (int i = 0; i < transform.ChildCount; i++)
+      {
+         
+      }
+   }
+}
